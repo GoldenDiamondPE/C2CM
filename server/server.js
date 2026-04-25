97f981086b23c733b8daed3c2473a63ac0aeeb16
GoldenDiamondPE <brandonbula@gmail.com>
2026-04-25 09:26:37 -0400

    Fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Express Setup
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // Cors Setup
 const cors = require("cors");
@@ -10,10 +11,17 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
 app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "strawberry", "pineapple", "grape", "turtle"] });
 });
 
 app.listen(8080, () => {
   console.log("Server started on port 8080");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
