17fbe3038bd678fc60ea7758f727d09a80365e33
GoldenDiamondPE <brandonbula@gmail.com>
2026-04-28 11:50:09 -0400

    Fixed server to work on Azure

--- server/server.js
+++ server/server.js
@@ -1,44 +1,36 @@
-// Express Setup
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+
 const PORT = process.env.PORT || 8080;
 
-
 console.log("SERVER FILE IS RUNNING");
+
+// Logging
 app.use((req, res, next) => {
   console.log("Incoming request:", req.method, req.url);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("ROOT WORKS");
-});
-
-// Cors Setup
-const cors = require("cors");
-const corsOptions = {
-  origin: ["http://localhost:5173", "coursestocareerpathmapperwa-e5gbh3grh6a6fxbj.eastus-01.azurewebsites.net"],
+// CORS
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://coursestocareerpathmapperwa-e5gbh3grh6a6fxbj.eastus-01.azurewebsites.net"
+  ],
   optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+}));
 
-// Serve static files from React app
+// Static React build
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
+// API
 app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "strawberry", "pineapple", "grape", "turtle"] });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
-
+// Debug route
 app.get("/test-file", (req, res) => {
   const fs = require("fs");
   const filePath = path.join(__dirname, "../client/dist/index.html");
@@ -47,4 +39,14 @@ app.get("/test-file", (req, res) => {
     exists: fs.existsSync(filePath),
     path: filePath
   });
+});
+
+// React fallback (LAST)
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
