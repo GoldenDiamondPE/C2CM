569fd0987246f6c20a257b4c65340f822953b643
kevinmurphy4533 <kevinmurphy4533@gmail.com>
2026-05-20 20:40:12 -0400

    Student View Initial Layout Idea
    An idea for the layout of the student view including three basic menu placeholders for now.

--- client/src/pages/Student.tsx
+++ client/src/pages/Student.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-
+import '../style/App.css';
 export default function Home() {
   return (
     <div>
@@ -7,6 +7,18 @@ export default function Home() {
         <title>Student | C2CM</title>
       </Helmet>
       <h1 className='text-center text-4xl font-bold mb-5'>Student</h1>
+
+      <div className="mx-auto container">
+        <div className="mx-auto mt-15 w-full max-w-lg rounded-xl p-6 text-black border-8 border-psuBeaver">
+          <p className="text-3xl font-bold text-center pb-5">Completed Courses</p>
+        </div>
+        <div className="mx-auto mt-15 w-full max-w-lg rounded-xl p-6 text-black border-8 border-psuBeaver">
+          <p className="text-3xl font-bold text-center pb-5">Planned Courses</p>
+        </div>
+        <div className="mx-auto mt-15 w-full max-w-lg rounded-xl p-6 text-black border-8 border-psuBeaver">
+          <p className="text-3xl font-bold text-center pb-5">Recommended Courses</p>
+        </div>
+      </div>
 
     </div>
   );
