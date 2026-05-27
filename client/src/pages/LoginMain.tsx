12b579067dec1878350e3d35dbb4c3c380644d8c
kevinmurphy4533 <kevinmurphy4533@gmail.com>
2026-05-27 17:17:14 -0400

    Minor Azure Fix

--- client/src/pages/LoginMain.tsx
+++ client/src/pages/LoginMain.tsx
@@ -6,14 +6,16 @@ export default function Home() {
       <Helmet>
         <title>Login | C2CM</title>
       </Helmet>
-      <h1 className='text-center text-4xl font-bold mb-5'>Course to Career Path Mapper Login</h1>
+      <h1 className='text-center text-4xl font-bold mb-5'>Course to Career Path Mapper</h1>
+
+      <p className='text-center text-2xl mb-5'>Welcome to login page for the Course to Career Path Mapper. To use this site please navigate to one of the login options below.</p>
 
       <div className="mx-auto flex justify-center items-center min-h-50 gap-55 p-4">
-            <Link to="/studentSetup" className="text-3xl font-semibold text-center pb-5 bg-psuBeaver hover:bg-psuNittany px-6 py-3 rounded-lg inline-block transition text-white">Student Login</Link>
+            <Link to="/studentSetup" className="text-3xl font-semibold text-center pb-5 bg-psuBeaver hover:bg-psuNittany px-10 py-3 rounded-lg inline-block transition text-white">Student Login</Link>
 
-            <Link to="/faculty" className="text-3xl font-semibold text-center pb-5 bg-psuBeaver hover:bg-psuNittany px-6 py-3 rounded-lg inline-block transition text-white">Faculty Login</Link>
+            <Link to="/faculty" className="text-3xl font-semibold text-center pb-5 bg-psuBeaver hover:bg-psuNittany px-10 py-3 rounded-lg inline-block transition text-white">Faculty Login</Link>
 
-            <Link to="/admin" className="text-3xl font-semibold text-center pb-5 bg-psuBeaver hover:bg-psuNittany px-6 py-3 rounded-lg inline-block transition text-white">Admin Login</Link>
+            <Link to="/admin" className="text-3xl font-semibold text-center pb-5 bg-psuBeaver hover:bg-psuNittany px-10 py-3 rounded-lg inline-block transition text-white">Admin Login</Link>
       </div>
 
     </div>
